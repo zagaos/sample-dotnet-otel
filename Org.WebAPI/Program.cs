2a21bbfaa718cce06b488e016ddf72a478bfe434
ZAGA Opensource <53016349+zagaos@users.noreply.github.com>
2023-08-31 21:40:40 -0500

    Update Program.cs

--- Org.WebAPI/Program.cs
+++ Org.WebAPI/Program.cs
@@ -11,23 +11,49 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddOpenTelemetryTracing(budiler =>
+builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
 {
-    budiler
-        .AddAspNetCoreInstrumentation(opt =>
-        {
-            opt.RecordException = true;
-        })
-        .SetResourceBuilder(ResourceBuilder.CreateDefault()
+    openTelemetryLoggerOptions.SetResourceBuilder(
+        ResourceBuilder.CreateEmpty()
             .AddService("Org.WebAPI")
-            .AddTelemetrySdk()
-        )
-        .SetErrorStatusOnException(true)
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317");
-        });
-});
+            .AddAttributes(new Dictionary<string, object>
+            {
+                ["deployment.environment"] = "development"
+            }));
+        
+    openTelemetryLoggerOptions.IncludeScopes = true;
+    openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+    
+    openTelemetryLoggerOptions.AddOtlpExporter(exporter =>
+    {
+       // exporter.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //exporter.Endpoint = new Uri("https://seq.example.com/ingest/otlp/v1/logs");
+        //exporter.Headers = "X-Seq-ApiKey=fd8sHZ28fajva8t32Ngjfdisp";
+        option.Protocol = OtlpExportProtocol.Grpc;
+            // option.Endpoint = new Uri("https://otel-custom-grpc-otel.apps.cluster-hvnhl.hvnhl.sandbox2235.opentlc.com"); // Expose gRPC endpoint as route doesn't work!!!
+            option.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317"); // Only gRPC service endpoint works, 
+            option.ExportProcessorType = ExportProcessorType.Batch;
+    });
+}));
+
+// builder.Services.AddOpenTelemetryTracing(budiler =>
+// {
+//     budiler
+//         .AddAspNetCoreInstrumentation(opt =>
+//         {
+//             opt.RecordException = true;
+//         })
+//         .SetResourceBuilder(ResourceBuilder.CreateDefault()
+//             .AddService("Org.WebAPI")
+//             .AddTelemetrySdk()
+//         )
+//         .SetErrorStatusOnException(true)
+//         .AddOtlpExporter(options =>
+//         {
+//             options.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317");
+//         });
+// });
+
 // builder.Logging.ClearProviders();
 // builder.Logging.AddOpenTelemetry(options =>
 // {
@@ -45,23 +71,7 @@ builder.Services.AddOpenTelemetryTracing(budiler =>
 //         });
 // });
 
-builder.Services.AddOpenTelemetryTracing(budiler =>
-{
-    budiler
-        .AddAspNetCoreInstrumentation(opt =>
-        {
-            opt.RecordException = true;
-        })
-        .SetResourceBuilder(ResourceBuilder.CreateDefault()
-            .AddService("Org.WebAPI")
-            .AddTelemetrySdk()
-        )
-        .SetErrorStatusOnException(true)
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri("http://localhost:4317"); // Signoz Endpoint
-        });
-});
+
 
 var app = builder.Build();
 

487af0efdcb278b3ea00a497c75ca37bdd6c501e
ZAGA Opensource <53016349+zagaos@users.noreply.github.com>
2023-08-31 21:54:12 -0500

    Update Program.cs

--- Org.WebAPI/Program.cs
+++ Org.WebAPI/Program.cs
@@ -5,36 +5,35 @@ using OpenTelemetry.Trace;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Logging.ClearProviders();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
-{
-    openTelemetryLoggerOptions.SetResourceBuilder(
-        ResourceBuilder.CreateEmpty()
-            .AddService("Org.WebAPI")
-            .AddAttributes(new Dictionary<string, object>
-            {
-                ["deployment.environment"] = "development"
-            }));
+// builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
+// {
+//     openTelemetryLoggerOptions.SetResourceBuilder(
+//         ResourceBuilder.CreateEmpty()
+//             .AddService("Org.WebAPI")
+//             .AddAttributes(new Dictionary<string, object>
+//             {
+//                 ["deployment.environment"] = "development"
+//             }));
         
-    openTelemetryLoggerOptions.IncludeScopes = true;
-    openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+//     openTelemetryLoggerOptions.IncludeScopes = true;
+//     openTelemetryLoggerOptions.IncludeFormattedMessage = true;
     
-    openTelemetryLoggerOptions.AddOtlpExporter(exporter =>
-    {
-       // exporter.Protocol = OtlpExportProtocol.HttpProtobuf;
-        //exporter.Endpoint = new Uri("https://seq.example.com/ingest/otlp/v1/logs");
-        //exporter.Headers = "X-Seq-ApiKey=fd8sHZ28fajva8t32Ngjfdisp";
-        exporter.Protocol = OtlpExportProtocol.Grpc;
-            // option.Endpoint = new Uri("https://otel-custom-grpc-otel.apps.cluster-hvnhl.hvnhl.sandbox2235.opentlc.com"); // Expose gRPC endpoint as route doesn't work!!!
-            exporter.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317"); // Only gRPC service endpoint works, 
-            exporter.ExportProcessorType = ExportProcessorType.Batch;
-    });
-}));
+//     openTelemetryLoggerOptions.AddOtlpExporter(exporter =>
+//     {
+//        // exporter.Protocol = OtlpExportProtocol.HttpProtobuf;
+//         //exporter.Endpoint = new Uri("https://seq.example.com/ingest/otlp/v1/logs");
+//         //exporter.Headers = "X-Seq-ApiKey=fd8sHZ28fajva8t32Ngjfdisp";
+//         exporter.Protocol = OtlpExportProtocol.Grpc;
+//             // option.Endpoint = new Uri("https://otel-custom-grpc-otel.apps.cluster-hvnhl.hvnhl.sandbox2235.opentlc.com"); // Expose gRPC endpoint as route doesn't work!!!
+//             exporter.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317"); // Only gRPC service endpoint works, 
+//             exporter.ExportProcessorType = ExportProcessorType.Batch;
+//     });
+// }));
 
 // builder.Services.AddOpenTelemetryTracing(budiler =>
 // {
@@ -54,22 +53,22 @@ builder.Services.AddLogging(logging => logging.AddOpenTelemetry(openTelemetryLog
 //         });
 // });
 
-// builder.Logging.ClearProviders();
-// builder.Logging.AddOpenTelemetry(options =>
-// {
-//     options
-//         .SetResourceBuilder(ResourceBuilder.CreateDefault()
-//             .AddService("Org.WebAPI")
-//             .AddEnvironmentVariableDetector()
-//             .AddTelemetrySdk()
-//         )
-//         // .SetErrorStatusOnException()
-//         .AddConsoleExporter()
-//         .AddOtlpExporter(opt =>
-//         {
-//             opt.Endpoint = new Uri("http://localhost:4317"); // Signoz Endpoint
-//         });
-// });
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("Org.WebAPI")
+            .AddEnvironmentVariableDetector()
+            .AddTelemetrySdk()
+        )
+        // .SetErrorStatusOnException()
+        .AddConsoleExporter()
+        .AddOtlpExporter(opt =>
+        {
+            opt.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317"); // Signoz Endpoint
+        });
+});
 
 
 

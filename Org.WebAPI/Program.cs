4a8ccc8142f2dfc3f47dc1f7a23fc31bb495ea58
ZAGA Opensource <53016349+zagaos@users.noreply.github.com>
2023-08-31 21:52:28 -0500

    Update Program.cs

--- Org.WebAPI/Program.cs
+++ Org.WebAPI/Program.cs
@@ -5,7 +5,7 @@ using OpenTelemetry.Trace;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Logging.ClearProviders();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
